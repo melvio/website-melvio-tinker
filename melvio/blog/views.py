e6d8bee718802c21b8a1d055afe51aea3e49e384
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-22 19:17:50 +0100

    added extra blog url

--- melvio/blog/views.py
+++ melvio/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# view on homepage of the blog
+def home(request):
+    return HttpResponse("<h1>Home Blog</h1>")
+
+
+def about(request):
+    print(request)
+    return HttpResponse("<h2>It's e me!</h2>")
