b18f03ef32159015bfda53107b02d69beb690934
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-23 14:47:29 +0100

    create context for home page

--- melvio/blog/apps.py
+++ melvio/blog/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    # add this 'blog' folder to the project
     name = 'blog'
--- melvio/blog/views.py
+++ melvio/blog/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Melvin',
+        'title' : 'blog post',
+        'content': 'first post',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Melvin2',
+        'title': 'blog post2',
+        'content': '2nd post',
+        'date_posted': 'August 28, 2020'
+    }
+]
+
 
 # view on homepage of the blog
 def home(request):
+    context = {'posts': posts}
     return render(request=request,
-                  template_name='blog/home.html')
+                  template_name='blog/home.html',
+                  context=context)
 
 
 def about(request):
