8be9ae551967637485eb140476708ffaddb2054a
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-23 08:22:57 +0100

    using templates instead of inline html

--- melvio/blog/views.py
+++ melvio/blog/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 
 # view on homepage of the blog
 def home(request):
-    return HttpResponse("<h1>Home Blog</h1>")
+    return render(request=request,
+                  template_name='blog/home.html')
 
 
 def about(request):
-    print(request)
-    return HttpResponse("<h2>It's e me!</h2>")
+    return render(request=request,
+                  template_name='blog/about.html')
