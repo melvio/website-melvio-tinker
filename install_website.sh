f27c9ed4d70fbc9f2389dbfb1ffd018261bd25a1
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-27 17:37:47 +0100

    updates to website

--- install_website.sh
+++ install_website.sh
@@ -3,8 +3,10 @@
 #git clone https://github.com/melvio/website-melvio.git
 #cd website-melvio/ || exit 1
 
-sudo apt-get update -y
-sudo apt-get install -y python python3-pip virtualenv git
+sudo apt-get update  -y
+sudo apt-get upgrade -y
+sudo apt-get install -y python python3-pip virtualenv
+sudo apt-get install -y git nginx-full
 
 
 if [[ ! -d venv/ ]] ; then
@@ -14,8 +16,9 @@ source ./venv/bin/activate
 
 python3 -m pip install --upgrade -r requirements.txt
 
-python3 melvio/manage.py migrate
-python3 melvio/manage.py runserver 127.0.0.1:80
+cd melvio/ || exit 1
+python3 manage.py migrate
+gunicorn --bind localhost:8000 melvio.wsgi:application
 
 
 
