50a7f8374a8e39ff90ec1b18dcae7ec31a7d0be2
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-27 16:35:41 +0100

    update install website

--- install_website.sh
+++ install_website.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+#git clone https://github.com/melvio/website-melvio.git
+#cd website-melvio/ || exit 1
+
 sudo apt-get update -y
 sudo apt-get install -y python python3-pip virtualenv git
 
-git clone https://github.com/melvio/website-melvio.git
-cd website-melvio/ || exit 1
 
 if [[ ! -d venv/ ]] ; then
   python3 -m virtualenv venv
-  source ./venv/bin/activate
 fi
+source ./venv/bin/activate
 
 python3 -m pip install --upgrade -r requirements.txt
 
+python3 melvio/manage.py migrate
 python3 melvio/manage.py runserver 127.0.0.1:80
 
 
